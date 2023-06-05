2f9c4bda1149a0e32f1b58ec8c02a5528d065891
David <montanezdavid@gmail.com>
2023-06-04 23:55:11 -0500

    Configured Reset Password Page

---
+++ src/pages/ResetPassword/resetPassword.js
@@ -0,0 +1,17 @@
+const ResetPassword = () => {
+    return (
+        <>
+            <h1>Reset Password</h1>
+
+            <input
+                placeholder="Email Address"
+            />
+
+            <button>Send</button>
+
+            <p>Already have an account? SIGN IN</p>
+        </>
+    );
+}
+ 
+export default ResetPassword;
