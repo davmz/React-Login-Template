4abc392368ba4cf7d0f31a1bfd422d0d159437e0
David <montanezdavid@gmail.com>
2023-06-04 23:54:59 -0500

    Configured Sign Up Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SignIn from './pages/SignIn/signin';
+import SignUp from './pages/SignUp/signup';
+import ResetPassword from './pages/ResetPassword/resetPassword';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="reset-password" element={<ResetPassword />} />
       </Routes>    
     </BrowserRouter>
   );
---
+++ src/pages/SignUp/signup.js
@@ -0,0 +1,27 @@
+const SignUp = () => {
+    return (
+        <>
+            <h1>Sign Up</h1>
+
+            <input
+                placeholder="Full Name"
+            />
+
+            <input
+                placeholder="Email Address"
+            />
+
+            <input
+                placeholder="Password"
+            />
+
+            <input
+                placeholder="Repeat Password"
+            />
+
+            <button>Sign Up</button>
+        </>
+    );
+}
+ 
+export default SignUp;
