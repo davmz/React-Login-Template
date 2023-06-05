12ed7e69ab7dbe10ed65bb18685551af08a2e9f5
David <montanezdavid@gmail.com>
2023-06-04 23:23:36 -0500

    Installed react router dom and configured /signin page

---
+++ src/pages/SignIn/signin.js
@@ -0,0 +1,22 @@
+const SignIn = () => {
+    return (
+        <>
+            <h1>Sign In</h1>
+            <input
+                placeholder="Email Address"
+            />
+
+            <input
+                placeholder="Password"
+            />
+
+            <p>Forgot Password?</p>
+
+            <button>Sign In</button>
+
+            <p>Don't have an account? Register Now</p>
+        </>
+    );
+}
+ 
+export default SignIn;
